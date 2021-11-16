e3115e383e433119772d4bbc1c44614a87468006
OskarKozaczka <oskaro0@vp.pl>
2021-11-16 22:21:21 +0100

    .

--- .NetApp/AIModel.cs
+++ .NetApp/AIModel.cs
@@ -1,69 +1,83 @@
-﻿using static Tensorflow.Binding;
-using static Tensorflow.KerasApi;
-using Tensorflow;
-using Tensorflow.NumPy;
-using System;
+﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Tensorflow.Keras.Layers;
-using System.IO;
 using Newtonsoft.Json;
+using Numpy;
+using Keras.Models;
+using Keras.Layers;
+using Keras;
 
 namespace project
 {
     public class AIModel
     {
-
-        void test()
+        static void start()
         {
-            var layers = new LayersApi();
-            var inputs = keras.Input(shape: (10, 10), name: "img");
+
+            var model = createModel();
+            NDarray x;
+            NDarray y;
+            dataPreparation(out x, out y);
+
+            model.Fit(x, y, batch_size: 2, epochs: 100, verbose: 1);
+
+            saveModel(model);
+
+            var test = np.array(JsonConvert.DeserializeObject<int[,]>(File.ReadAllLines("data/journal/OOHSD.txt")[0].Split(';')[0])).reshape(1, 10, 10);
+
+            model.Predict(test);
+        }
+
+        public static Sequential createModel()
+        {
+            var model = new Sequential();
+            model.Add(new Dense(100, activation: "relu", input_shape: new Shape(10, 10)));
+            model.Add(new Dense(64, activation: "relu"));
+            model.Add(new Dense(1, activation: "sigmoid"));
+            model.Add(new Flatten());
+
+            model.Compile(optimizer: "sgd", loss: "binary_crossentropy", metrics: new string[] { "accuracy" });
+            model.Summary();
+            return model;
+        }
+        public class Move
+        {
+            public int x { set; get; }
+            public int y { set; get; }
+        }
+
+        public static void dataPreparation(out NDarray x,out NDarray y)
+        {
+            var features = new List<NDarray>();
+            var labels = new List<int>();
 
 
-
-            var x = layers.Conv2D(32, 3, activation: "relu").Apply(inputs);
-            x = layers.Conv2D(64, 3, activation: "relu").Apply(x);
-            var block_1_output = layers.MaxPooling2D(3).Apply(x);
-            x = layers.Conv2D(64, 3, activation: "relu", padding: "same").Apply(block_1_output);
-            x = layers.Conv2D(64, 3, activation: "relu", padding: "same").Apply(x);
-            var block_2_output = layers.Add().Apply(new Tensors(x, block_1_output));
-            x = layers.Conv2D(64, 3, activation: "relu", padding: "same").Apply(block_2_output);
-            x = layers.Conv2D(64, 3, activation: "relu", padding: "same").Apply(x);
-            var block_3_output = layers.Add().Apply(new Tensors(x, block_2_output));
-            x = layers.Conv2D(64, 3, activation: "relu").Apply(block_3_output);
-            x = layers.GlobalAveragePooling2D().Apply(x);
-            x = layers.Dense(256, activation: "relu").Apply(x);
-            x = layers.Dropout(0.5f).Apply(x);
-            var outputs = layers.Dense(100).Apply(x);
-
-            var model = keras.Model(inputs, outputs, name: "toy_resnet");
-            model.summary();
-            model.compile(optimizer: keras.optimizers.RMSprop(1e-3f),
-            loss: keras.losses.CategoricalCrossentropy(from_logits: true),
-            metrics: new[] { "acc" });
-
-
-            var features = new List<string>();
-            var labels = new List<string>();
-            var lines = File.ReadAllLines("data/journal/*.csv");
-            foreach(var line in lines )
+            var lines = File.ReadAllLines("data/journal/OOHSD.txt");
+            foreach (var line in lines)
             {
-                features.Append(line.Split(';')[0]);
-                labels.Append(line.Split(';')[1]);
+                features.Add(np.array(JsonConvert.DeserializeObject<int[,]>(line.Split(';')[0])));
+                Move move = JsonConvert.DeserializeObject<Move>(line.Split(';')[1]);
+                labels.Add(int.Parse(move.x.ToString() + move.y.ToString()));
             }
 
-          model.fit(np.array(features.ToArray()), np.array(labels.ToArray()),
-          batch_size: 1,
-          epochs: 10,
-          validation_split: 0.2f);
-
-
-            var test = new Tensor(np.array(JsonConvert.DeserializeObject<int[,]>(File.ReadAllLines("data/journal/*.csv")[0].Split(';')[0])));
-            model.predict(test);
-
-
+            x = np.array(features.ToArray());
+            y = np.array(labels.ToArray());
         }
-    }
+        public static void saveModel(Sequential model)
+        {
+            string json = model.ToJson();
+            File.WriteAllText("model.json", json);
+            model.SaveWeight("model.h5");
+        }
 
+        public static BaseModel LoadModel()
+        {
+            var loaded_model = Sequential.ModelFromJson(File.ReadAllText("model.json"));
+            loaded_model.LoadWeight("model.h5");
+            return loaded_model;
+        }
+
+    }
 }
+   
